2a385b670733fb39a1dd0352e0808be44fe4a95f
Yoshiaki Araki <yoshiaki.araki@G-PC-00495736.intra.gree-office.net>
2019-09-06 16:26:49 +0900

    index, self

--- sample.js
+++ sample.js
@@ -1,10 +1,26 @@
 'use strict';
-import unfold from './lib/unfold.js';
+import arrayformula from './lib/arrayformula.js';
+import vlookup from './lib/vlookup.js';
+import sortby from './lib/sortby.js';
 
-const numbers = Array.from(new Array(100).keys()).map(i => ++i);// 1..100の連番の配列生成
-const result = numbers                               // 配列を...
-  .filter(number => number % 3 !== 0)                // 何かの条件で絞って...
-  .map(number => number * 2)                         // 加工して...
-  .filter(number => number <= 100)                   // さらに絞って...
-  .reduce((sum, number) => sum + number);            // 合計していると。
-console.log(result);// 1734
+
+let A = ['c','a','b','c','c','a'];
+let B = [['a','b','c'],[1,2,3]];
+//console.log(arrayformula([A,B],(a,b)=>
+//  sortby(a,vlookup(a,b,1,false)))
+//);
+console.log(vlookup('c',B,1,false));
+
+console.log(arrayformula(A,
+  (a) => vlookup(a,B,1,false)
+));
+
+console.log(arrayformula([A,B],
+  (a,b,index,_A,_B) => vlookup(a,_B,1,false)
+));
+
+//  =sortby(
+// {"c";"a";"b";"c";"c";"a"},
+// vlookup({"c";"a";"b";"c";"c";"a"}, {"a",1;"b",2;"c",3},2,false),
+console.log(sortby(A, arrayformula([A,B],
+  (a,b,index,_A,_B) => vlookup(a,_B,1,false)),true));

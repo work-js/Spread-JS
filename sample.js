0410d5ecec9aeaba6ca8010e9424291c37f3805b
Yoshiaki Araki <yoshiaki.araki@G-PC-00495736.intra.gree-office.net>
2019-09-10 11:36:19 +0900

    move sample

--- sample.js
+++ sample.js
@@ -5,7 +5,7 @@ import sortby from './lib/sortby.js';
 import sort from './lib/sort.js';
 import rows from './lib/rows.js';
 import array_constrain from './lib/array_constrain.js';
-
+import formula from './lib/formula.js';
 
 let A = ['c','a','b','c','c','a'];
 let B = [['a','b','c'],[1,2,3]];
@@ -28,6 +28,15 @@ console.log(arrayformula([A,B],
 console.log(sortby(A, arrayformula([A,B],
   (a,b,index,_A,_B) => vlookup(a,_B,1,false)),true));
 
+console.log(
+  formula(
+    [A,B],
+    (x,y)=>sortby(x, 
+      arrayformula(x,(a) => vlookup(a,y,1,false)
+      ),true)));
+// [ 'a', 'a', 'b', 'c', 'c', 'c' ]
+    
+
 //  =sortby(
 // {"c";"a";"b";"c";"c";"a"},
 // vlookup({"c";"a";"b";"c";"c";"a"}, {"a",1;"b",2;"c",3},2,false),
@@ -36,3 +45,5 @@ console.log(sortby(A, arrayformula(A,
 
 console.log(array_constrain(sort([A, arrayformula(A,
   (a) => vlookup(a,B,1,false))],1,true),rows(A),1));
+
+
